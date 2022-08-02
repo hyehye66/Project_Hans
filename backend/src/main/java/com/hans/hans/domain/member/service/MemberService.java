4304ed7bb7898ab529ae176438104c3e5ad730e0
신예인 <syi0511@naver.com>
2022-08-02 15:31:19 +0900

    HS-83/back/feature/member/get 사용자는 회원정보를 확인할 수 있다.

---
+++ backend/src/main/java/com/hans/hans/domain/member/service/MemberService.java
@@ -0,0 +1,17 @@
+package com.hans.hans.domain.member.service;
+
+
+import com.hans.hans.domain.member.dto.MemberRequestDto;
+import com.hans.hans.domain.member.dto.MemberResponseDto;
+import com.hans.hans.domain.member.dto.MemberUpdateRequestDto;
+
+public interface MemberService {
+
+    MemberResponseDto signUpMember(MemberRequestDto memberRequestDto, String refreshToken);
+    MemberResponseDto updateMemberInfo(String email, MemberUpdateRequestDto memberUpdateRequestDto);
+    MemberResponseDto getMemberInfo(String email);
+    void deleteMember(String email);
+
+    void updateRefreshToken(String email, String refreshToken);
+
+}
