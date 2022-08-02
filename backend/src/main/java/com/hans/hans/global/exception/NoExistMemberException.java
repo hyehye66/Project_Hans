732eaef3211032d7300d211662200acf8982ca7d
신예인 <syi0511@naver.com>
2022-08-02 15:45:51 +0900

    HS-89/back/feature/member/update 사용자는 회원정보를 수정할 수 있다.

---
+++ backend/src/main/java/com/hans/hans/global/exception/NoExistMemberException.java
@@ -0,0 +1,7 @@
+package com.hans.hans.global.exception;
+
+public class NoExistMemberException extends RuntimeException{
+    public NoExistMemberException(String message){
+        super(message);
+    }
+}
