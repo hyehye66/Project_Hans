df26ac27843a4eaf50b8ad7c35feacbb4bc28897
syi0511 <syi0511@naver.com>
2022-08-04 15:21:32 +0900

    [refactor] : MemberService 메서드명 수정

--- backend/src/main/java/com/hans/hans/domain/member/service/MemberService.java
+++ backend/src/main/java/com/hans/hans/domain/member/service/MemberService.java
@@ -7,7 +7,7 @@ import com.hans.hans.domain.member.dto.MemberUpdateRequestDto;
 
 public interface MemberService {
 
-    MemberResponseDto signUpMember(MemberRequestDto memberRequestDto, String refreshToken);
+    MemberResponseDto createMember(MemberRequestDto memberRequestDto, String refreshToken);
     MemberResponseDto updateMemberInfo(String email, MemberUpdateRequestDto memberUpdateRequestDto);
     MemberResponseDto getMemberInfo(String email);
     void deleteMember(String email);
