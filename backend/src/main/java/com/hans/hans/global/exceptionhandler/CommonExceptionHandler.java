9fa2c7bb5dc9804dfe5d739b57726a05d79e083f
syi0511 <syi0511@naver.com>
2022-08-08 00:32:04 +0900

    [feat] : NoExistRoomException Handler 생성

--- backend/src/main/java/com/hans/hans/global/exceptionhandler/CommonExceptionHandler.java
+++ backend/src/main/java/com/hans/hans/global/exceptionhandler/CommonExceptionHandler.java
@@ -2,6 +2,7 @@ package com.hans.hans.global.exceptionhandler;
 
 import com.hans.hans.global.exception.ExistNicknameException;
 import com.hans.hans.global.exception.NoExistMemberException;
+import com.hans.hans.global.exception.NoExistRoomException;
 import com.hans.hans.global.response.CommonResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +23,12 @@ public class CommonExceptionHandler {
     // 존재하는 회원이 없는 에러 Handler
     @ExceptionHandler(NoExistMemberException.class)
     public ResponseEntity<CommonResponse> handleNoExistMemberException(RuntimeException e){
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(CommonResponse.createError(e.getMessage()));
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(CommonResponse.createError(e.getMessage()));
+    }
+
+    @ExceptionHandler(NoExistRoomException.class)
+    public ResponseEntity<CommonResponse> handleNoExistRoomException(RuntimeException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(CommonResponse.createError(e.getMessage()));
     }
 
     // validation 유효성 실패 에러 Handler
@@ -30,5 +36,6 @@ public class CommonExceptionHandler {
     public ResponseEntity<CommonResponse> handleMethodArgumentNotValidException(BindingResult bindingResult){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(CommonResponse.createFail(bindingResult));
     }
+
 
 }
