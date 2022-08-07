391eea9b19ea24795fa6e2299ef3b70482c23500
syi0511 <syi0511@naver.com>
2022-08-08 00:35:56 +0900

    [fix] : MemberService 수정

--- backend/src/main/java/com/hans/hans/domain/member/service/MemberService.java
+++ backend/src/main/java/com/hans/hans/domain/member/service/MemberService.java
@@ -1,16 +1,13 @@
 package com.hans.hans.domain.member.service;
 
 
-import com.hans.hans.domain.member.dto.MemberRequestDto;
-import com.hans.hans.domain.member.dto.MemberResponseDto;
-import com.hans.hans.domain.member.dto.MemberSignUpResponseDto;
-import com.hans.hans.domain.member.dto.MemberUpdateRequestDto;
+import com.hans.hans.domain.member.dto.*;
 
 public interface MemberService {
 
-    MemberSignUpResponseDto createMember(MemberRequestDto memberRequestDto, String refreshToken);
+    MemberResponseDto createMember(MemberRequestDto memberRequestDto, String refreshToken);
     MemberResponseDto updateMemberInfo(String email, MemberUpdateRequestDto memberUpdateRequestDto);
-    MemberResponseDto getMemberInfo(String email);
+    MemberInfoResponseDto getMemberInfo(String email);
     void deleteMember(String email);
 
     void updateRefreshToken(String email, String refreshToken);
