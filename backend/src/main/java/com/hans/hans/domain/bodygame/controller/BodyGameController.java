d487d3192823d5b0c9016bd270be3db0d72d07fb
신예인 <syi0511@naver.com>
2022-08-08 21:22:10 +0900

    Fix/body-game/get BodyGameController mode sequence 변경

--- backend/src/main/java/com/hans/hans/domain/bodygame/controller/BodyGameController.java
+++ backend/src/main/java/com/hans/hans/domain/bodygame/controller/BodyGameController.java
@@ -8,6 +8,7 @@ import com.hans.hans.domain.room.dto.RoomGetRequestDto;
 import com.hans.hans.domain.room.dto.RoomMemberResponseDto;
 import com.hans.hans.domain.room.dto.RoomsResponseDto;
 import com.hans.hans.domain.room.service.RoomService;
+import com.hans.hans.global.enumerate.Modes;
 import com.hans.hans.global.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -28,7 +29,7 @@ public class BodyGameController {
 
     @GetMapping()
     public ResponseEntity<?> getBodyGameRooms(@PageableDefault(size = 6) Pageable pageable){
-        RoomsResponseDto roomsResponseDto = roomService.getRooms(RoomGetRequestDto.builder().modeSequence(3).build(), pageable);
+        RoomsResponseDto roomsResponseDto = roomService.getRooms(RoomGetRequestDto.builder().modeSequence(Modes.BODY.getModeSequence()).build(), pageable);
 
         return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.createSuccess("몸으로 말해요 게임방 리스트 조회에 성공하였습니다. ", roomsResponseDto));
     }
