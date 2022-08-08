c8b06c1d8a8f72a9f4835c34bf1acc27cb20014c
신예인 <syi0511@naver.com>
2022-08-08 10:14:07 +0900

    fix/login/google-oauth 구글 인증 로직 삭제

--- backend/src/main/java/com/hans/hans/domain/login/service/LoginServiceImpl.java
+++ backend/src/main/java/com/hans/hans/domain/login/service/LoginServiceImpl.java
@@ -21,58 +21,6 @@ import java.net.URI;
 @RequiredArgsConstructor
 public class LoginServiceImpl implements LoginService {
 
-    @Value("${google.client.id}")
-    private String GOOGLE_CLIENT_ID;
-
-    @Value("${google.client.secret}")
-    private String GOOGLE_CLIENT_SECRET;
-
-    @Value("${google.redirect.uri}")
-    private String GOOGLE_REDIRECT_URI;
-
-    private final String GOOGLE_GRANT_TYPE = "authorization_code";
-
-    private final JwtService jwtService;
-
-    @Override
-    //Authorization code -> accessToken, refreshToken 받아오기
-    public GoogleTokenResponseDto getGoogleAccessToken(String code){
-        URI uri = UriComponentsBuilder.fromUriString("https://oauth2.googleapis.com")
-                .path("/token")
-                .encode()
-                .build()
-                .toUri();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        MultiValueMap<String,String> params = new LinkedMultiValueMap<>();
-        params.add("code",code);
-        params.add("client_id",GOOGLE_CLIENT_ID);
-        params.add("client_secret",GOOGLE_CLIENT_SECRET);
-        params.add("redirect_uri",GOOGLE_REDIRECT_URI);
-        params.add("grant_type",GOOGLE_GRANT_TYPE);
-
-        HttpEntity<MultiValueMap<String,String>> restRequest = new HttpEntity<>(params,headers);
-        return new RestTemplate().postForEntity(uri,restRequest, GoogleTokenResponseDto.class).getBody();
-    }
-
-    @Override
-    //accessToken -> userInfo의 email 받아오기
-    public String getGoogleEmail(String accessToken){
-        URI uri = UriComponentsBuilder.fromUriString("https://www.googleapis.com")
-                .path("/oauth2/v3/userinfo")
-                .encode()
-                .build()
-                .toUri();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(accessToken);
-
-        HttpEntity<HttpHeaders> request = new HttpEntity<>(headers);
-
-        return new RestTemplate().exchange(uri, HttpMethod.GET, request, GoogleMemberInfoResponseDto.class).getBody().getEmail();
-    }
-
     @Override
     public HttpHeaders createTokenHeader(String accessToken, String refreshToken) {
 
