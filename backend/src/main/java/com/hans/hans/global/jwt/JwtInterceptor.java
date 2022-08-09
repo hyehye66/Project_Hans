f589874397857ffe8b1dd70f5f7e6c38e6c2bbdf
신예인 <syi0511@naver.com>
2022-08-09 10:59:24 +0900

    Refactor/jwt/interceptor JwtInterceptor 리팩토링 작업

--- backend/src/main/java/com/hans/hans/global/jwt/JwtInterceptor.java
+++ backend/src/main/java/com/hans/hans/global/jwt/JwtInterceptor.java
@@ -18,15 +18,14 @@ public class JwtInterceptor implements HandlerInterceptor  {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-        String accessToken = request.getHeader("Authorization").replace("Bearer ","");
-        String refreshToken = request.getHeader("refreshToken");
-
         if("OPTIONS".equals(request.getMethod())){
             return true;
         }
 
         try{
-            if(accessToken!=null){
+            if(request.getHeader("Authorization")!=null){
+                String accessToken = request.getHeader("Authorization").replace("Bearer ","");
+                String refreshToken = request.getHeader("refreshToken");
                 if(jwtService.validateToken(accessToken)){
                     String email = jwtService.getEmailFromPayload(accessToken);
                     request.setAttribute("email",email);
@@ -47,8 +46,6 @@ public class JwtInterceptor implements HandlerInterceptor  {
                 }
                 throw new JwtException("유효하지 않은 Access Token 입니다.");
             }else{
-                response.sendRedirect("/api/login");
-                response.setStatus(401);
                 throw new NotLoggedInException("로그인한 사용자만 접근가능합니다.");
             }
         }catch (IllegalArgumentException | JwtException e) {
