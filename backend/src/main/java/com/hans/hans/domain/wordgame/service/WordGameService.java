799bc907898565e7b445970a6e91eebde56db063
신예인 <syi0511@naver.com>
2022-08-10 13:47:54 +0900

    HS-248/back/feature/word-game/result/get  사용자는 낱말 게임이 끝나면 게임 참여자들의 점수와 등수를 최종적으로 볼 수 있다.

--- backend/src/main/java/com/hans/hans/domain/wordgame/service/WordGameService.java
+++ backend/src/main/java/com/hans/hans/domain/wordgame/service/WordGameService.java
@@ -3,7 +3,9 @@ package com.hans.hans.domain.wordgame.service;
 
 import com.hans.hans.domain.wordgame.dto.WordGameAnswerRequestDto;
 import com.hans.hans.domain.wordgame.dto.WordGameAnswerResponseDto;
+import com.hans.hans.domain.wordgame.dto.WordGameResultResponseDto;
 
 public interface WordGameService {
     WordGameAnswerResponseDto getAnswer(WordGameAnswerRequestDto wordGameAnswerRequestDto, Long roomSequence);
+    WordGameResultResponseDto getResult(Long roomSequence);
 }
