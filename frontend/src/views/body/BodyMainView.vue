08506c9adcc3c5d10e1db485ff5e39e094c772fb
우영택 <do0134@naver.com>
2022-08-10 17:57:11 +0900

    S07P12D109-368 [feat] : 사용자는 랭킹 페이지에서 전체 랭킹을 볼 수 있다.

--- frontend/src/views/body/BodyMainView.vue
+++ frontend/src/views/body/BodyMainView.vue
@@ -4,9 +4,22 @@
     <br>
     <button>+ 방 생성</button>
     <button>빠른 입장</button>
-    <button>?</button>    
+    <button>?</button>  
+
+    <div class="flex justify-center">
+      <div class="mb-3 xl:w-96">
+        <div class="input-group relative flex flex-wrap items-stretch w-full mb-4 rounded">
+          <input type="search" class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
+          <span class="input-group-text flex items-center px-3 py-1.5 text-base font-normal text-gray-700 text-center whitespace-nowrap rounded" id="basic-addon2">
+            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+            </svg>
+          </span>
+        </div>
+      </div>
+    </div>  
     
-  <form>   
+  <!-- <form>   
       <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"></label>
       <div class="relative">
           <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
@@ -15,7 +28,7 @@
           <input type="search" id="search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required="">
           <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
       </div>
-  </form>
+  </form> -->
 
 
 
--- frontend/src/views/rank/components/RankTab.vue
+++ frontend/src/views/rank/components/RankTab.vue
@@ -3,51 +3,220 @@
     <div class="w-full">
       <ul class="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row">
         <li class="-mb-px mr-2 last:mr-0 flex-auto text-center" style="cursor:pointer">
-          <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(1)" v-bind:class="{'text-blue-500 bg-white': openTab !== 1, 'text-white bg-blue-500': openTab === 1}">
+          <a class="category-tab text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" 
+            v-on:click="toggleTabs(1)" 
+            v-bind:class="{'text-newOrange bg-white': openTab !== 1, 
+            'text-white bg-newOrange': openTab === 1}"
+            @click="getRanking">
             <i class="fas fa-space-shuttle text-base mr-1"></i> 낱말게임
           </a>
         </li>
         <li class="-mb-px mr-2 last:mr-0 flex-auto text-center" style="cursor:pointer">
-          <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(2)" v-bind:class="{'text-blue-500 bg-white': openTab !== 2, 'text-white bg-blue-500': openTab === 2}">
+          <a class="category-tab text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" 
+            v-on:click="toggleTabs(2)" 
+            v-bind:class="{'text-blue-500 bg-white': openTab !== 2, 
+            'text-white bg-blue-500': openTab === 2}"
+            @click="getRanking">
             <i class="fas fa-cog text-base mr-1"></i> 몸으로 말해요
           </a>
         </li>
         <li class="-mb-px mr-2 last:mr-0 flex-auto text-center" style="cursor:pointer">
-          <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(3)" v-bind:class="{'text-blue-500 bg-white': openTab !== 3, 'text-white bg-blue-500': openTab === 3}">
+          <a class="category-tab text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" 
+            v-on:click="toggleTabs(3)" 
+            v-bind:class="{'text-blue-500 bg-white': openTab !== 3, 
+            'text-white bg-blue-500': openTab === 3}"
+            @click="getRanking">
             <i class="fas fa-briefcase text-base mr-1"></i> 가사퀴즈
           </a>
         </li>
       </ul>
 
-      <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
-        <div class="px-4 py-5 flex-auto">
-          <div class="tab-content tab-space">
+      <div class="tab-box relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded">
+       <!-- bg-white -->
+        <div class="px-4 py-5 flex-auto" style="width: 80%">
+          <div class="tab-content tab-space" >          
             <div v-bind:class="{'hidden': openTab !== 1, 'block': openTab === 1}">
-              <ul class="list-group" style="width: 750px" v-for="situation in situationSentence" :key="situation.situationSequence">
-              <li class="list-group-item" v-if="situation.category==='제목1'">
-                <one-dialog :sentence="situation.sentence"/>
-              </li>
-            </ul>
-            <!-- <ul class="list-group" style="width: 750px" v-for="rank in rankList" :key="rank.rankSequence">
-              <li class="list-group-item" v-if="rank.category==='제목1'">
-                <words-rank :rankList="rank.list"/>
-              </li>
-            </ul>   -->
+              <words-search-bar />
+
+              <!-- 새 리스트 -->
+              <div class="overflow-x-auto">
+                <table class="table table-zebra w-full">
+                  <!-- head -->
+                  <thead>
+                    <tr>
+                      <!-- <th></th> -->
+                      <th>순위</th>
+                      <th>닉네임</th>
+                      <th>점수</th>
+                      <th>티어</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <!-- row 1 -->
+                    <tr v-for="rankData in rankDataList" :key="rankData.rankingSequence">
+                      <th><rank-items :rankingSequence="rankData.rankingSequence"/></th>
+                      <td><rank-items :nickname="rankData.nickname"/></td>
+                      <td><rank-items :score="rankData.score" /></td>
+                      <td><rank-items :tier="rankData.tier" /></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
+
+
+
+
+
+
+
+              <!-- 기존 리스트 -->
+              <!-- <ul class="title-list-group" style="width: 750px">
+                <li class="title-list-group-items">
+                  <b class="title-list-group-item">순위</b>
+                  <b class="title-list-group-item">닉네임</b>
+                  <b class="title-list-group-item">점수</b>
+                  <b class="title-list-group-item">티어</b>
+                </li>
+              </ul>
+              <ul class="list-group" style="width: 750px" v-for="rankData in rankDataList" :key="rankData.rankingSequence">
+                <li class="list-group-item">
+                  <rank-items :rankingSequence="rankData.rankingSequence"/>                  
+                  <rank-items :nickname="rankData.nickname"/>                  
+                  <rank-items :score="rankData.score" />            
+                  <rank-items :tier="rankData.tier" />
+                </li>
+              </ul>
+            </div> -->
+            <!--  -->
+
+              <!-- <table id="table-group" cellspacing="0" cellpadding="0">
+                <thead>
+                  <tr>
+                    <th><span>순위</span></th>
+                    <th><span>닉네임</span></th>
+                    <th><span>점수</span></th>
+                    <th><span>티어</span></th>
+                  </tr>
+                </thead>
+                <tbody class="list-group" style="width: 750px" 
+                    v-for="rankData in rankDataList" 
+                    :key="rankData.rankingSequence">
+                  <tr>
+                    <td class="lalign"><rank-items :rankingSequence="rankData.rankingSequence" /></td>
+                    <td><rank-items :nickname="rankData.nickname" /></td>
+                    <td><rank-items :score="rankData.score" /></td>
+                    <td><rank-items :tier="rankData.tier" /></td>
+                  </tr>
+                </tbody>
+              </table> -->
+
+              
+              <!-- <div class="table-title-group">
+                <ul class="list-group" style="width: 750px">
+                  <li class="table-title-group-item">
+                    <span>순위</span>            
+                    <span>닉네임</span>                              
+                    <span>점수</span>                              
+                    <span>티어</span>                                          
+                  </li>
+                </ul>
+              </div> -->
+
+              <!-- <ul class="list-group" style="width: 750px" v-for="rankData in rankDataList" :key="rankData.rankingSequence">
+                <li class="list-group-item">
+                  <rank-items :rankingSequence="rankData.rankingSequence" />            
+                  <rank-items :nickname="rankData.nickname" />            
+                  <rank-items :score="rankData.score" />            
+                  <rank-items :tier="rankData.tier" />                  
+                </li>
+              </ul> -->
+            
+
+            
             <div v-bind:class="{'hidden': openTab !== 2, 'block': openTab === 2}">
-            <ul class="list-group" style="width: 750px" v-for="situation in situationSentence" :key="situation.situationSequence">
-              <li class="list-group-item" v-if="situation.category==='제목2'">
-                <one-dialog :sentence="situation.sentence"/>
-              </li>
-            </ul>  
+              <body-search-bar />
+              <div class="overflow-x-auto">
+                <table class="table table-zebra w-full">
+                  <!-- head -->
+                  <thead>
+                    <tr>
+                      <!-- <th></th> -->
+                      <th>순위</th>
+                      <th>닉네임</th>
+                      <th>점수</th>
+                      <th>티어</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <!-- row 1 -->
+                    <tr v-for="rankData in rankDataList" :key="rankData.rankingSequence">
+                      <th><rank-items :rankingSequence="rankData.rankingSequence"/></th>
+                      <td><rank-items :nickname="rankData.nickname"/></td>
+                      <td><rank-items :score="rankData.score" /></td>
+                      <td><rank-items :tier="rankData.tier" /></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
-            <div v-bind:class="{'hidden': openTab !== 3, 'block': openTab === 3}">
-            <ul class="list-group" style="width: 750px" v-for="situation in situationSentence" :key="situation.situationSequence">
-              <li class="list-group-item" v-if="situation.category==='제목3'">
-                <one-dialog :sentence="situation.sentence"/>
-              </li>
-            </ul>  
+
+
+
+              <!-- <ul class="list-group" style="width: 750px" v-for="rankData in rankDataList" :key="rankData.rankingSequence">
+                <li class="list-group-item">
+                  <rank-items :rankingSequence="rankData.rankingSequence"/>                  
+                  <rank-items :nickname="rankData.nickname"/>                  
+                  <rank-items :score="rankData.score" />            
+                  <rank-items :tier="rankData.tier" />
+                </li>
+              </ul>  
+            </div> -->
+
+
+            <div v-bind:class="{'hidden': openTab !== 3, 'block': openTab === 3}" @click="getRanking()">
+              <body-search-bar />
+              <div class="overflow-x-auto">
+                <table class="table table-zebra w-full">
+                  <!-- head -->
+                  <thead>
+                    <tr>
+                      <!-- <th></th> -->
+                      <th>순위</th>
+                      <th>닉네임</th>
+                      <th>점수</th>
+                      <th>티어</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <!-- row 1 -->
+                    <tr v-for="rankData in rankDataList" :key="rankData.rankingSequence">
+                      <th><rank-items :rankingSequence="rankData.rankingSequence"/></th>
+                      <td><rank-items :nickname="rankData.nickname"/></td>
+                      <td><rank-items :score="rankData.score" /></td>
+                      <td><rank-items :tier="rankData.tier" /></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
+
+
+
+
+
+
+              <!-- <ul class="list-group" style="width: 750px" v-for="rankData in rankDataList" :key="rankData.rankingSequence">
+                <li class="list-group-item">
+                  <rank-items :rankingSequence="rankData.rankingSequence"/>
+                  <rank-items :nickname="rankData.nickname"/>
+                  <rank-items :score="rankData.score" />            
+                  <rank-items :tier="rankData.tier" />
+                </li>
+              </ul>  
+            </div> -->
+                
+
           </div>
         </div>
       </div>
@@ -57,46 +226,81 @@
 </template>
 
 <script>
-import WordsRank from '@/views/rank/components/WordsRank.vue'
-import BodyRank from '@/views/rank/components/BodyRank.vue'
+
+import WordsSearchBar from '@/views/rank/components/WordsSearchBar.vue'
+import BodySearchBar from '@/views/rank/components/BodySearchBar.vue'
+import RankItems from '@/views/rank/components/RankItems.vue'
 import axios from 'axios';
+import {mapGetters} from 'vuex';
 
 export default {
   name: 'RankTab',
   components: {
-  
-},
-props : {
-  // situationSentence : Array
-},
+    WordsSearchBar,
+    BodySearchBar,
+    RankItems,  
+  },  
   data() {
     return {
       openTab: 1,
-      index: 0,
-      mode_pk: 1,
+      index: 0, 
+      rankDataList : [], 
     }
   },
+  // props : {
+  //   rankDataList : Array,
+  // },
   methods: {
     toggleTabs: function(tabNumber){
       this.openTab = tabNumber
     },
+    // firstShowRanking () {
+      
 
+    // },
+
+    // 탭을 누를 때마다 axios요청하기
     getRanking () {
-      axios.get (
-        `api/rankings/${this.mode_pk}?page=${this.index}`,
-        {auth:{
-          username: 'OPENVIDUAPP',
-          password: 'hans',
+      console.log(this.openTab, typeof(this.openTab))
+      axios ( {
+        url: `/api/rankings/${this.openTab+1}`,
+        // ?page=${this.index}
+        headers: this.authHeader,
+        method: "get",
+        // 수정
+        // modeSequence별로 데이터 받아오기
+        data: {
+          modeSequence: this.openTab+1,
+          
+        }
+      }
+        
 
-        }},
-      ).then (res=>console.log(res.data))      
+      )
+      // .then (res=>console.log(res.data))
 
+      //인자로 넣어주는 함수니 콜백함수. 함수가 메서드가 아니므로 this는 method다. 콜백함수는 무조건 화살표쓴다
+      //.then(res => this.photos = res.data ) //리턴 없고 인자도 하나니 이렇게 가능하다
+      // .then((res) => {
+      //   this.rankData = res.data.data
+      //   }) 
+      //   .catch(err => console.error(err)) 
+        
+      .then (res => {
+        this.rankDataList = res.data.data.rankings.content       
+        console.log(this.rankDataList)} )
+      
+      .catch(err => console.error(this.authHeader))               
+      
     },
 
 
   },
   created () {
     this.getRanking()
+  },
+  computed: {
+    ...mapGetters(["authHeader"])
   }
 
 }
@@ -107,5 +311,55 @@ props : {
   display: flex;
   justify-content: center;
   align-items: center;
+  width: 70%;
+  margin-top: 5%;
 }
+
+.category-tab {
+  background: #dfdfdf;
+  color: #f16b48;
+  font-size: 1.2em;
+}
+
+.category-tab:hover {
+  color: #f38e7b;
+}
+
+.tab-box {
+  /* background: #f16b48; */
+
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-self: center;
+}
+
+title-list-group {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  align-self: center;
+
+}
+
+words-search-bar {  
+  /* display: flex; */
+	/* flex-direction: row; */
+	/* flex-flow: row wrap; */
+	/* justify-content: space-evenly; */
+	/* align-self: center; */
+	/* justify-content: center; */
+	/* text-align: center; */
+	/* width: 400;
+	height: 40; */
+  /* float: right; */
+
+}
+
+/* search-rank svg {
+  vertical-align: middle;
+} */
+
+
+
 </style>
