33c6f970481b2984e4a8156fc021dcb0dd10f817
syi0511 <syi0511@naver.com>
2022-08-10 14:55:00 +0900

    [feat] : WordGameSocketController 최종 결과 기능 추가

--- backend/src/main/java/com/hans/hans/domain/wordgame/controller/WordGameSocketController.java
+++ backend/src/main/java/com/hans/hans/domain/wordgame/controller/WordGameSocketController.java
@@ -1,10 +1,7 @@
 package com.hans.hans.domain.wordgame.controller;
 
-import com.hans.hans.domain.wordgame.dto.WordGameAnswerRequestDto;
-import com.hans.hans.domain.wordgame.dto.WordGameAnswerResponseDto;
+import com.hans.hans.domain.wordgame.dto.*;
 import com.hans.hans.domain.wordgame.service.WordGameSocketService;
-import com.hans.hans.domain.wordgame.dto.WordGameStartRequestDto;
-import com.hans.hans.domain.wordgame.dto.WordGameStartResponseDto;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -19,17 +16,24 @@ public class WordGameSocketController {
     private final SimpMessageSendingOperations sendingOperations;
     private final WordGameSocketService wordGameSocketService;
 
-    @MessageMapping("/game/answer/{room_seq}")
-    @SendTo("/topic/game/answer/{room_seq}")
-    public void getAnswer(WordGameAnswerRequestDto wordGameAnswerRequestDto, @DestinationVariable("room_seq") Long roomSequence){
-        WordGameAnswerResponseDto wordGameAnswerResponseDto = wordGameSocketService.getAnswer(wordGameAnswerRequestDto, roomSequence);
-        //sendingOperations.convertAndSend(wordGameAnswerResponseDto);
-    }
-
     @MessageMapping("/word-game/{room_seq}")
     @SendTo("/topic/word-game/{room_seq}")
     public WordGameStartResponseDto initGame(@DestinationVariable("room_seq") long roomSequence, WordGameStartRequestDto wordGameStartRequestDto) {
         return wordGameSocketService.initGame(roomSequence, wordGameStartRequestDto);
+    }
+
+    @MessageMapping("/game/answer/{room_seq}")
+    @SendTo("/topic/game/answer/{room_seq}")
+    public void getAnswer(WordGameAnswerRequestDto wordGameAnswerRequestDto, @DestinationVariable("room_seq") Long roomSequence){
+        WordGameAnswerResponseDto wordGameAnswerResponseDto = wordGameSocketService.getAnswer(wordGameAnswerRequestDto, roomSequence);
+        sendingOperations.convertAndSend(wordGameAnswerResponseDto);
+    }
+
+    @MessageMapping("game/result/{room_seq}")
+    @SendTo("/topic/game/result/{room_seq}")
+    public void getResult(@DestinationVariable("room_seq") Long roomSequence){
+        WordGameResultResponseDto wordGameResultResponseDto = wordGameSocketService.getResult(roomSequence);
+        sendingOperations.convertAndSend(wordGameResultResponseDto);
     }
 
 }
