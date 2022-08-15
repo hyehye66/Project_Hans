efc9acfddce87b6847defce03f3c213416a21267
syi0511 <syi0511@naver.com>
2022-08-15 22:34:05 +0900

    [fix] : JsonAutoDetect

--- backend/src/main/java/com/hans/hans/domain/wordgame/dto/WordGameOwnerRequestDto.java
+++ backend/src/main/java/com/hans/hans/domain/wordgame/dto/WordGameOwnerRequestDto.java
@@ -1,10 +1,13 @@
 package com.hans.hans.domain.wordgame.dto;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@JsonAutoDetect
 public class WordGameOwnerRequestDto {
     private int totalQuestion;
 }
