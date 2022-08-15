6e4e7771915cf9561ecc2d7c181b617913a6b303
syi0511 <syi0511@naver.com>
2022-08-15 22:47:38 +0900

    [fix] : jsonproperty 추가

--- backend/src/main/java/com/hans/hans/domain/wordgame/dto/WordGameOwnerRequestDto.java
+++ backend/src/main/java/com/hans/hans/domain/wordgame/dto/WordGameOwnerRequestDto.java
@@ -1,6 +1,7 @@
 package com.hans.hans.domain.wordgame.dto;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 public class WordGameOwnerRequestDto {
+    @JsonProperty("totalQuestion")
     private int totalQuestion;
 }
