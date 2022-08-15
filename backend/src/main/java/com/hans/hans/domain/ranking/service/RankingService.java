acdbab5cf10d663f7df8c9fa94f4f23ad580d475
류인석 <inseok9876@gmail.com>
2022-08-15 21:45:49 +0900

    HS-589/back/feature/ranking/search 사용자는 랭킹 검색시 자신의 랭킹을 볼 수 있다.

--- backend/src/main/java/com/hans/hans/domain/ranking/service/RankingService.java
+++ backend/src/main/java/com/hans/hans/domain/ranking/service/RankingService.java
@@ -1,18 +1,16 @@
 package com.hans.hans.domain.ranking.service;
 
-import com.hans.hans.domain.member.entity.Member;
-import com.hans.hans.domain.mode.entity.Mode;
 import com.hans.hans.domain.ranking.dto.RankingRequestDto;
 import com.hans.hans.domain.ranking.dto.RankingResponseByMemberDto;
+import com.hans.hans.domain.ranking.dto.RankingSearchResponseDto;
 import com.hans.hans.domain.ranking.dto.RankingsResponseDto;
-import com.hans.hans.domain.ranking.entity.Ranking;
 import com.hans.hans.global.enumerate.Modes;
 import org.springframework.data.domain.Pageable;
 
 public interface RankingService {
-    RankingsResponseDto getRankingListByMode(int id, Pageable pageable);
+    RankingsResponseDto getRankingListByMode(int modeSequence, Pageable pageable);
     RankingResponseByMemberDto createRanking(RankingRequestDto rankingRequestDto);
-    RankingResponseByMemberDto searchRanking(String nickname, int id);
+    RankingSearchResponseDto searchRanking(String nickname, int modeSequence);
     void updateRanking(String nickname, Modes modes, Long score);
 
 }
--- backend/src/main/java/com/hans/hans/domain/ranking/service/RankingServiceImpl.java
+++ backend/src/main/java/com/hans/hans/domain/ranking/service/RankingServiceImpl.java
@@ -4,14 +4,10 @@ import com.hans.hans.domain.member.entity.Member;
 import com.hans.hans.domain.member.repository.MemberRepository;
 import com.hans.hans.domain.mode.entity.Mode;
 import com.hans.hans.domain.mode.repository.ModeRepository;
-import com.hans.hans.domain.ranking.dto.RankingRequestDto;
-import com.hans.hans.domain.ranking.dto.RankingResponseByMemberDto;
-import com.hans.hans.domain.ranking.dto.RankingResponseDto;
-import com.hans.hans.domain.ranking.dto.RankingsResponseDto;
+import com.hans.hans.domain.ranking.dto.*;
 import com.hans.hans.domain.ranking.entity.Ranking;
 import com.hans.hans.domain.ranking.repository.RankingRepository;
 import com.hans.hans.global.enumerate.Modes;
-import com.hans.hans.global.exception.NoExistMemberException;
 import com.hans.hans.global.exception.NoExistRankingListSearchByNicknameException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -29,8 +25,8 @@ public class RankingServiceImpl implements RankingService{
     private final MemberRepository memberRepository;
 
     @Override
-    public RankingsResponseDto getRankingListByMode(int id, Pageable pageable) {
-        Mode mode = modeRepository.findByModeSequence(id);
+    public RankingsResponseDto getRankingListByMode(int modeSequence, Pageable pageable) {
+        Mode mode = modeRepository.findByModeSequence(modeSequence);
         Page<Ranking> list = rankingRepository.findRankingsByModeOrderByScoreDesc(mode, pageable);
         // RankingResponseDto로 변환
         Page<RankingResponseDto> map = list.map(ranking -> new RankingResponseDto(ranking));
@@ -46,16 +42,20 @@ public class RankingServiceImpl implements RankingService{
     }
 
     @Override
-    public RankingResponseByMemberDto searchRanking(String nickname, int id) {
+    public RankingSearchResponseDto searchRanking(String nickname, int modeSequence) {
         Member member = memberRepository.findByNickname(nickname);
 
-        Mode mode = modeRepository.findByModeSequence(id);
+        Mode mode = modeRepository.findByModeSequence(modeSequence);
         Ranking ranking = rankingRepository.findRankingByMemberAndMode(member, mode);
+        
         if(ranking==null) throw new NoExistRankingListSearchByNicknameException("현재 검색한 닉네임의 사용자는 랭킹 리스트에 등록되어있지 않습니다.");
 
-        RankingResponseByMemberDto rankingResponseByMemberDto = new RankingResponseByMemberDto(ranking);
+        List<Ranking> lists = rankingRepository.findRankingsByModeOrderByScoreDesc(mode);
+        int rank = lists.indexOf(ranking)+1;
 
-        return rankingResponseByMemberDto;
+        
+        RankingSearchResponseDto rankingSearchResponseDto = new RankingSearchResponseDto(ranking,rank);
+        return rankingSearchResponseDto;
     }
 
     @Override
