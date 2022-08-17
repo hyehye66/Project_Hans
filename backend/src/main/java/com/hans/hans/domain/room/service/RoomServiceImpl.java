8898d257939fd7d60f16329460936e9787ed5318
신예인 <syi0511@naver.com>
2022-08-17 10:49:38 +0900

    HS-595/back/feature/conversation/sort 방 리스트를 생성 일자 내림차순으로 Response 해준다.

--- backend/src/main/java/com/hans/hans/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/hans/hans/domain/room/service/RoomServiceImpl.java
@@ -64,7 +64,7 @@ public class RoomServiceImpl implements RoomService{
 
     @Override
     public RoomsResponseDto getRooms(RoomGetRequestDto roomGetRequestDto, Pageable pageable) {
-        Page<Room> rooms = roomRepository.findRoomsByMode(roomGetRequestDto.toEntity(), pageable);
+        Page<Room> rooms = roomRepository.findRoomsByModeOOrderByRoomDTTMDesc(roomGetRequestDto.toEntity(), pageable);
         RoomsResponseDto roomsResponseDto = new RoomsResponseDto(rooms);
 
         return roomsResponseDto;
